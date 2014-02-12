782a4924f8641759dc1c83fef2572ffad9a524c2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-12 10:21:08 +0100

    include xautolock recipe

--- cookbooks/dvb-industries/recipes/default.rb
+++ cookbooks/dvb-industries/recipes/default.rb
@@ -1,8 +1,9 @@
 basis = %w{emacs i3 fish caps_lock}
 mathematics = %w{latex geogebra coq proofgeneral}
 languages = %w{node haskell prolog scheme java leiningen}
+tools = %w{xautolock}
 
-recipes = [basis, mathematics, languages]
+recipes = [basis, mathematics, languages, tools]
 
 recipes.flatten.each do |a_recipe|
   include_recipe a_recipe
