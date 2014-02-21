2a0b228844f3b6e1640f7cbd59f4f9f5986da3e5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-21 12:54:03 +0100

    add eclipse recipe

--- cookbooks/dvb-industries/recipes/default.rb
+++ cookbooks/dvb-industries/recipes/default.rb
@@ -1,7 +1,7 @@
 basis = %w{emacs i3 fish caps_lock}
 mathematics = %w{latex geogebra coq proofgeneral}
 languages = %w{node haskell prolog scheme java leiningen}
-tools = %w{xautolock imagemagick}
+tools = %w{xautolock imagemagick eclipse}
 
 recipes = [basis, mathematics, languages, tools]
 
